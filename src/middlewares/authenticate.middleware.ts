ed4b0496bba8a3333892f58b45b63034657f2ab7
Victhor Hugo <victhor.souza@picpay.com>
2024-05-26 21:15:31 -0300

    refactor: Adicionando tratamento de erro para token inválido

--- src/middlewares/authenticate.middleware.ts
+++ src/middlewares/authenticate.middleware.ts
@@ -4,7 +4,13 @@ import 'dotenv/config';
 export function authenticateToken(req, res, next) {
     const { headers } = req;
     const token = headers.authorization.split(" ")[1];
-    const { sub } = verify(token, process.env.TOKEN_SECRET);
-    req.body.userId = sub;
-    next();
+    try{
+        var { sub } = verify(token, process.env.TOKEN_SECRET);
+        req.body.userId = sub;
+        next();
+    }catch(err){
+        res.status(403).json({
+            error: 'Token inválido'
+        })
+    }
 }
--- src/modules/notes/use-cases/notes.controller.ts
+++ src/modules/notes/use-cases/notes.controller.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router: Router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send("Bem vindo as notas")
+    res.send("Bem vindo as notas");
 });
 
 module.exports = router;
