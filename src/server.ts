530f3efe6e30f84ffad8690f0606bc400a0e1fd0
Victhor Hugo <victhor.souza@picpay.com>
2024-06-09 22:53:53 -0300

    refactor: Atribuição da porta'

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 const express = require('express')
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const userRouter = require('./modules/user/use-cases/user.controller');
 const notesRouter = require('./modules/notes/use-cases/notes.controller');
@@ -9,14 +9,14 @@ const notesRouter = require('./modules/notes/use-cases/notes.controller');
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Hello World. Wellcome to My Notes API!")
+    res.send("Hello World. Welcome to My Notes API!")
 });
 
 app.use('/user', userRouter);
 app.use('/notes', notesRouter);
 
 export function initServer(): void {
-    app.listen(port || 3000, '0.0.0.0', () => {
+    app.listen(port, '0.0.0.0', () => {
         console.log("Servidor rodando na porta: ", port)
     });
 }
