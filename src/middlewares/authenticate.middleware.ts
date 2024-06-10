04c2df4dcf36c3920a3c1f50a0327d0838d8d01b
Victhor Hugo <victhor.souza@picpay.com>
2024-06-09 23:34:03 -0300

    fix: corrigindo authenticateToken middleware

--- src/middlewares/authenticate.middleware.ts
+++ src/middlewares/authenticate.middleware.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 export function authenticateToken(req, res, next) {
     const { headers } = req;
-    const token = headers.authorization.split(" ")[1];
+    const token = headers.authorization?.split(" ")[1];
     try{
         var { sub } = verify(token, process.env.TOKEN_SECRET);
         req.body.userId = sub;
